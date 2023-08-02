81bef5d91e88e1fe370a0ba8c378ab55faaffcde
HigeMan <kohei.otani@tutanota.com>
2023-08-02 21:43:42 +0900

    Hailing User Input Tutorial done

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Kohei Otani on 2023/08/02.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button{
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
